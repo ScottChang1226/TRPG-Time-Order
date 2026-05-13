30369cd73261cbe8edc236ea3de40ba69d60f39e
ScottChang1226 <ScottChang1226@users.noreply.github.com>
2026-05-13 06:45:07 +0000

    deploy: 7f0f8a061891cdf8edb4ea10cdd5c23d89ed9b0d

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey:            "",
-  authDomain:        "",
-  projectId:         "",
-  storageBucket:     "",
-  messagingSenderId: "",
-  appId:             ""
+  apiKey:            "AIzaSyCWi_Yzw4evImrpK_J87be65nSgwY7Mb7Q",
+  authDomain:        "trpg-time-order.firebaseapp.com",
+  projectId:         "trpg-time-order",
+  storageBucket:     "trpg-time-order.firebasestorage.app",
+  messagingSenderId: "760875174988",
+  appId:             "1:760875174988:web:c38b1ceb6a92c0f57978e9"
 };
